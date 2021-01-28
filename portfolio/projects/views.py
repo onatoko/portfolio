8c58c4306513b67bc76489864da14f5e4711b049
onatoko <kfonatoko@gmail.com>
2021-01-27 21:38:46 -0500

    Create Projects page

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def home(request):
+    context = {"projects": Project.objects.all()}
+    return render(request, "projects/home.html", context)
