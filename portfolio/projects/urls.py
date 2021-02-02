d8548150fba9318cb8d99141fdd9cb4d68b91576
onatoko <kfonatoko@gmail.com>
2021-02-01 21:19:13 -0500

    Test class based view

--- portfolio/projects/urls.py
+++ portfolio/projects/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
+from .views import ProjectListView
 
-urlpatterns = [path("", views.home, name="portfolio-home")]
+urlpatterns = [
+    path("", views.home, name="portfolio-home"),
+    path("projects/", ProjectListView.as_view(), name="portfolio-projects"),
+]
--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Project
 
 
 def home(request):
     context = {"projects": Project.objects.all()}
     return render(request, "projects/home.html", context)
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "projects/home.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(ImagePostList)
