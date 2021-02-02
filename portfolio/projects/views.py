c302f7c3e06790e188f188be8f53d9aa8cdbf355
onatoko <kfonatoko@gmail.com>
2021-02-01 22:03:05 -0500

    Update ProjectListView

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -11,10 +11,11 @@ def home(request):
 class ProjectListView(ListView):
     model = Project
     template_name = "projects/home.html"
+    context_object_name = projects
 
-    def get_queryset(self):
-        projects = Project.objects.all()
-        return projects
+    # def get_queryset(self):
+    #     projects = Project.objects.all()
+    #     return projects
 
     # def get_context_data(self, **kwargs):
     #     context = super(ImagePostList)
