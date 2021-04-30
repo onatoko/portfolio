b1e8cec14dd05433b878bb6a677c70a4a410b82e
onatoko <kfonatoko@gmail.com>
2021-04-29 22:15:32 -0400

    Update admin.py

--- portfolio/projects/admin.py
+++ portfolio/projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Category, Project
+from .models import Category, Project, Image, ImageAlbum
 
 admin.site.register(Category)
 admin.site.register(Project)
+admin.site.register(Image)
+admin.site.register(ImageAlbum)
